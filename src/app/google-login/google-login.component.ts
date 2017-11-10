6d2e59e51f1c6dc4d252eccfa60411ac29acd992
sasangachathumal <chathumal.sasanga02@gmail.com>
2017-11-10 15:00:11 +0530

    added google sign out button and share button.

--- src/app/google-login/google-login.component.ts
+++ src/app/google-login/google-login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, NgZone } from '@angular/core';
+import {Component, OnInit, NgZone} from '@angular/core';
+
+declare const gapi: any;
 
 @Component({
   selector: 'app-google-login',
@@ -6,6 +8,9 @@ import { Component, OnInit, NgZone } from '@angular/core';
   styleUrls: ['./google-login.component.css']
 })
 export class GoogleLoginComponent implements OnInit {
+
+  isGoogleLogin: boolean = false;
+  logInUserData: any;
 
   constructor(private ngZone: NgZone) {
     window['onSignIn'] = (data) => ngZone.run(() => this.onSignIn(data));
@@ -16,10 +21,30 @@ export class GoogleLoginComponent implements OnInit {
 
   onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
+    var id_token = googleUser.getAuthResponse().id_token;
+    this.isGoogleLogin = true;
+    this.logInUserData = profile;
+    console.log('id_token: ', id_token);
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+
+  signOut() {
+    let self = this;
+    let auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+      self.change();
+    });
+  }
+
+  change() {
+    console.log("change",this.isGoogleLogin);
+    this.isGoogleLogin = false;
+    this.logInUserData = "";
+    console.log("change after",this.isGoogleLogin);
   }
 
 }
